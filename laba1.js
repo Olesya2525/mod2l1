5124a089e884140d0aba132231daa7edaf497bec
Olesya2525 <olesya.ryabets@icloud.com>
2025-02-16 19:55:26 +0400

    Add files via upload

---
+++ laba1.js
@@ -0,0 +1,18 @@
+"use strict";
+const multiplyTwoNumbers = (a, b) => a * b;
+console.log(multiplyTwoNumbers(5, 4));
+const constNumber = 22;
+const constString = "Пример строки";
+const constBoolean = true;
+let letObject = { key: "value" };
+let letArray = [1, 2, 3];
+let letNull = null;
+const constSymbol = Symbol("уникальный");
+console.log(constNumber, constString, constBoolean, letObject, letArray, letNull, constSymbol);
+const data = {
+    id: 4,
+    e1: null,
+    e2: null,
+};
+const jsonString = JSON.stringify(data);
+console.log(jsonString);
